8177efc5cd7768b3398151dc91ff9a50f99cca9d
Drakkars <drakkars@twitter>
2017-03-05 16:04:07 +0100

    AWS Stack: vpc + subnet + gw + route, elb + sg + listener + stickiness, 1 instance + sg + subnet + data

--- main.tf
+++ main.tf
@@ -82,6 +82,73 @@ resource "aws_security_group" "default" {
   }
 }
 
+# Our elb security group to access
+# the ELB over HTTP
+resource "aws_security_group" "elb" {
+  name        = "${var.stack_name}_elb_sg"
+  description = "Used in the terraform"
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ensure the VPC has an Internet gateway or this step will fail
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+resource "aws_elb" "web" {
+  name = "${var.stack_name}"
+
+  # The same availability zone as our instance
+  subnets = ["${aws_subnet.tf_test_subnet.id}"]
+
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+
+  instances                   = ["${aws_instance.web.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+resource "aws_lb_cookie_stickiness_policy" "default" {
+  name                     = "lbpolicy"
+  load_balancer            = "${aws_elb.web.id}"
+  lb_port                  = 80
+  cookie_expiration_period = 600
+}
+
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "elb_dns" {
+  value = "${aws_elb.web.dns_name}"
+}
+
 output "instance_ip" {
   value = "${aws_instance.web.public_ip}"
 }
